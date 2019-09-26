a1bd391ff8e2b59065b8a3447a78ed4a1d0eb583
EdMwa <emddb@mst.edu>
2019-09-25 19:09:07 -0500

    Add files via upload

---
+++ SortMapper.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 18:28:06 2019
+
+@author: JAB
+"""
+
+
+# !/bin/python
+''' Mapper for sort '''
+import  sys 
+for line in  sys.stdin: 
+    print(line.strip())
